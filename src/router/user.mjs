897190e9a91510c9a04a0f4de6de6b72cafac6cb
mohammad <mohammadheidari90900@gmail.com>
2025-02-10 23:07:33 +0330

    getUser

--- src/router/user.mjs
+++ src/router/user.mjs
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { users } from "../utility/constant";
+import { users } from "../utility/constant.mjs";
 
 
 const userRouter = Router();
@@ -12,5 +12,28 @@ userRouter.get("/",(req,res)=>{
 })
 
 
+userRouter.get("/users",(req,res)=>{
+    const {id} = req.query;
+
+    const user = checkingId(id)
+
+    if(user)
+        return res.json(user)
+    else
+        return res.json({message:"not exist"})
+
+})
+
+
 
 export default userRouter
+
+
+function checkingId (id){
+    const result = users.find(value=>value.id === parseInt(id))
+    if(result)
+        return result
+    else
+        return false
+}
+
---
+++ src/utility/constant.mjs
@@ -0,0 +1,10 @@
+export const users = [
+    {id:1,userName:"mohammad01",password:"123456",products:["phone","milk"]},
+    {id:2,userName:"ali13",password:"123456",products:["dull","laptop"]},
+    {id:3,userName:"javad34",password:"123456",products:["cable","mouse"]},
+    {id:4,userName:"lamine",password:"123456",products:[]},
+    {id:5,userName:"abbas",password:"123456",products:[]},
+    {id:6,userName:"hossein",password:"123456",products:[]},
+    {id:7,userName:"amirHossein",password:"123456",products:[]},
+    {id:9,userName:"navid",password:"123456",products:[]},
+]
