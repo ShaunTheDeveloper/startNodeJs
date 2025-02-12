e48b66e997d110242a36033599945b64fd33bc62
mohammad <mohammadheidari90900@gmail.com>
2025-02-12 20:15:11 +0330

    new functionality for user

--- src/router/user.mjs
+++ src/router/user.mjs
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import express from "express";
 import { users } from "../utility/constant.mjs";
 
 
 const userRouter = Router();
+
+userRouter.use(express.json())
+
+
+
 
 
 
@@ -13,7 +19,12 @@ userRouter.get("/",(req,res)=>{
 
 
 userRouter.get("/users",(req,res)=>{
-    const {id} = req.query;
+    return res.send(users)
+})
+
+
+userRouter.get("/users/:id",(req,res)=>{
+    const {id} = req.params;
 
     const user = checkingId(id)
 
@@ -22,6 +33,61 @@ userRouter.get("/users",(req,res)=>{
     else
         return res.json({message:"not exist"})
 
+})
+
+userRouter.get("/users/:id/products",(req,res)=>{
+    const id = parseInt(req.params.id);
+
+    const user = checkingId(id);
+    const index = findIndex(user);
+
+    return res.json(users[index].products);
+})
+
+
+userRouter.post("/users",(req,res)=>{
+    const {id,userName,password,products} = req.body
+
+    const user = checkingId(id);
+
+    if(user){
+        return res.json({message:"user with this id exist"})
+    }else{
+        const newUser = {id,userName,password,products}
+        users.push(newUser);
+        return res.json({message:"successful"})
+    }
+})
+
+
+userRouter.patch("/users/:id",(req,res)=>{
+    const id = parseInt(req.params.id)
+    const {userName,password,products} = req.body
+    const user = checkingId(id);
+
+    if(user){
+        const index = users.indexOf(user);
+        const updated = {id,userName:userName||users.userName,password:password||user.password,products:products||user.products}
+        users[index] = updated
+        return res.json(updated);
+    }else{
+        return res.json("not founded")
+    }
+})
+
+
+userRouter.delete("/users/:id",(req,res)=>{
+    const id  = parseInt(req.params.id);
+
+    const user = checkingId(id);
+
+    if(user){
+        const index= users.indexOf(user);
+        const deletedUser = users.splice(index,1);
+        return res.json(deletedUser)
+    }else{
+        return res.json("not founded")
+    }
 })
 
 
@@ -35,5 +101,12 @@ function checkingId (id){
         return result
     else
         return false
+}
+
+
+function findIndex(id){
+    const user = users.find((value)=>value.id===id)
+
+    return users.indexOf(user);
 }
 
