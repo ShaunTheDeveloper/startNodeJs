460a5a26dd3ba546da0501bfbd68b1eadced74d1
mohammad <mohammadheidari90900@gmail.com>
2025-03-20 19:11:42 +0330

    cookieParser

--- src/router/user/user.mjs
+++ src/router/user/user.mjs
@@ -3,21 +3,17 @@ import { Router } from "express";
 import { users } from "../../utility/constant.mjs";
 import { ifIdExist,ifIdNotExist,parsingId,checkValidationResult } from "./middleware.mjs";
 import {validateIdField,validateUserNameField,validatePasswordField} from "./validation.mjs";
-import { checkSchema } from "express-validator";
+import { checkSchema,matchedData } from "express-validator";
+import cookieParser, { signedCookie } from "cookie-parser";
 
 
 
 const userRouter = Router();
 
+
+
 userRouter.use(express.json())
-
-
-
-
-
-
-
-
+userRouter.use(cookieParser("secret"))
 
 
 
@@ -101,6 +97,30 @@ userRouter.delete("/users/id",
     const deletedUser = users.splice(index,1);
     return res.json(deletedUser)
     
+    
+})
+
+
+userRouter.post("/users/login/",checkSchema(validateUserNameField),checkSchema(validatePasswordField),checkValidationResult,(req,res)=>{
+    const {userName,password} = matchedData(req)
+    res.cookie("userName",userName,{maxAge:60000});
+    res.cookie("password",password,{maxAge:60000,signed:true})
+    return res.json("success")
+})
+
+
+
+userRouter.get("/users/login/status",(req,res)=>{
+    const {userName} = req.cookies;
+    const {password} = req.signedCookies;
+    console.log(userName);
+    console.log(password);
+
+    if(userName && password){
+        return res.json("you sign in to your account your id is ")
+    }else{
+        return res.json("you need to sign in to your account ")
+    }
     
 })
 
